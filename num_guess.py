473f73a5aa2e96da7d1fe00c23974d57bd758319
wunzt <wunzt@oregonstate.edu>
2022-04-12 19:08:37 -0700

    Assignment 3.3

--- num_guess.py
+++ num_guess.py
@@ -5,9 +5,9 @@ tracker = 1
 while guess != integer:
     tracker = tracker + 1
     if guess < integer:
-        guess = int(input("Too low - guess again:\n"))
+        guess = int(input("Too low - try again:\n"))
     elif guess > integer:
-        guess = int(input("Too high - guess again:\n"))
+        guess = int(input("Too high - try again:\n"))
 if tracker == 1:
     print("You guessed it in ", tracker, " try.")
 elif tracker > 1:
